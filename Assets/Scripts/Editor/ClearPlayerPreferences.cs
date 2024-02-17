5de3c9e359540325e936bfdc55d314c0c2987b93
StevenRice99 <Contact@StevenRice.ca>
2024-02-17 14:13:39 -0500

    Updated Unity version and added a script to clear player preferences.

---
+++ Assets/Scripts/Editor/ClearPlayerPreferences.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+// Editor only classes.
+namespace Editor
+{
+    /// <summary>
+    /// Class that exists in the editor only to clear player preferences.
+    /// </summary>
+    public static class ClearPlayerPreferences
+    {
+        /// <summary>
+        /// Clear player preferences from the menu under "Edit > Clear Player Preferences".
+        /// </summary>
+        [MenuItem("Edit/Clear Player Preferences", priority = int.MaxValue)]
+        private static void Perform()
+        {
+            // Delete all player preferences.
+            PlayerPrefs.DeleteAll();
+        }
+    }
+}
+#endif
